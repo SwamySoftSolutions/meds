55aa6965c25b69899a499d4b9c01f4669b1536fa
SwamySoftSolutions <nndsrinivas123@gmail.com>
2019-07-24 00:47:18 +0530

    edited

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=medpress',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
